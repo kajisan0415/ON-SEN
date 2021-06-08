88f4e6c50e4b70b1660deba7e0c358fad6447d1f
kajisan0415 <agonagakajisan0415@gmail.com>
2021-06-08 08:52:22 +0000

    ランキング機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,19 @@ devise_for :admin, :controllers => {
 
   scope module: :public do
     resources :blogs
-    resources :users,only: [:show,:edit,:update]
+    resources :users,only: [:show,:edit,:update] do
+      collection do
+        get :favorites
+      end
+    end
     resources :articles,only: [:index, :edit, :update, :destroy, :show] do
       resources :comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
       resource :stamps, only: [:create, :destroy]
     end
+    get "search" => "articles#search"
+    get "ranking" => "articles#ranking"
+    get "mapping" => "article#mapping"
   end
 
   root to: "public/homes#top"

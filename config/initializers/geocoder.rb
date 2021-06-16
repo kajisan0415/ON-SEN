da2b9d4d9a3868932f8a6d55b3d1d295f3bae007
kajisan0415 <agonagakajisan0415@gmail.com>
2021-06-16 12:41:02 +0000

    マップ機能完成

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,6 +2,6 @@ Geocoder.configure(
   # Geocoding options
   timeout: 5,                 # geocoding service timeout (secs)
   lookup: :google,         # name of geocoding service (symbol)
-  api_key: ENV['Google_Maps_API_Key'],               # API key for geocoding service
+  api_key: ENV['Geocoder_API_Key'],               # API key for geocoding service
   units: :km,                 # :km for kilometers or :mi for miles
 )
